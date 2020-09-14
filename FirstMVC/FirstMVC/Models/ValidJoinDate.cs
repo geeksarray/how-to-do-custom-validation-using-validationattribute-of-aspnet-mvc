a5aa08aa002acf9a41f1af38258c5c622540f8d5
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-09-14 11:16:35 +0530

    initial files

---
+++ FirstMVC/FirstMVC/Models/ValidJoinDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace FirstMVC.Models
+{
+    public class ValidJoinDate : ValidationAttribute
+    {      
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (Models.Customer)validationContext.ObjectInstance;
+            DateTime _dateJoin = Convert.ToDateTime(value); 
+            if (_dateJoin < DateTime.Now)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Join date can not be greater than current date.");
+            }
+        }        
+    }
+}
---
+++ FirstMVC/FirstMVC/Models/ValidLastDeliveryDate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstMVC.Models
+{
+    public class ValidLastDeliveryDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (Models.Customer)validationContext.ObjectInstance;
+            DateTime _lastDeliveryDate = Convert.ToDateTime(value);
+            DateTime _dateJoin = Convert.ToDateTime(model.JoinDate);  
+
+            if (_dateJoin > _lastDeliveryDate)
+            {
+                return new ValidationResult("Last Delivery Date can not be less than Join date.");                
+            }
+            else if (_lastDeliveryDate > DateTime.Now)
+            {
+                return new ValidationResult("Last Delivery Date can not be greater than current date.");                
+            }
+            else
+            {
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
